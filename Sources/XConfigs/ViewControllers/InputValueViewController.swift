64e09ff8370f4520472ef3a6947921268122a020
Michael Henry Pantaleon <me@iamkel.net>
2022-10-16 22:29:16 +1100

    update

--- Sources/XConfigs/ViewControllers/InputValueViewController.swift
+++ Sources/XConfigs/ViewControllers/InputValueViewController.swift
@@ -6,10 +6,27 @@ struct InputValueViewModel: ViewModelType {
     let title: String
     let value: String
 
-    typealias Input = Void
-    typealias Output = Void
+    struct Input {
+        let textPublisher: AnyPublisher<String, Never>
+        let dismissPublisher: AnyPublisher<Void, Never>
+        let donePublisher: AnyPublisher<Void, Never>
+    }
 
-    func transform(input _: Void) {}
+    struct Output {
+        let action: AnyPublisher<Action, Never>
+    }
+
+    enum Action {
+        case cancel
+        case done(String)
+    }
+
+    func transform(input: Input) -> Output {
+        let cancelAction = input.dismissPublisher.map { Action.cancel }
+        let doneAction = input.donePublisher.withLatestFrom(input.textPublisher).map(Action.done)
+        let action = Publishers.Merge(cancelAction, doneAction).eraseToAnyPublisher()
+        return .init(action: action)
+    }
 }
 
 final class InputValueViewController: UIViewController {
@@ -41,6 +58,7 @@ final class InputValueViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        handleViewModelOutput()
     }
 
     private func setupUI() {
@@ -57,21 +75,26 @@ final class InputValueViewController: UIViewController {
         view.backgroundColor = .systemBackground
         view.addSubview(textView)
         textView.bindToSuperview(margins: 20)
+    }
 
-        navigationItem.leftBarButtonItem?
-            .tapPublisher
-            .sink(receiveValue: { [weak self] _ in
+    private func handleViewModelOutput() {
+        guard let leftNavItem = navigationItem.leftBarButtonItem,
+              let rightNavITem = navigationItem.rightBarButtonItem
+        else { return }
+        let output = viewModel.transform(input: .init(
+            textPublisher: textView.textPublisher.compactMap { $0 }.eraseToAnyPublisher(),
+            dismissPublisher: leftNavItem.tapPublisher,
+            donePublisher: rightNavITem.tapPublisher
+        ))
+        output.action.sink { [weak self] action in
+            switch action {
+            case .cancel:
                 self?.dismiss(animated: true)
-            })
-            .store(in: &subscriptions)
-
-        navigationItem.rightBarButtonItem?
-            .tapPublisher
-            .sink(receiveValue: { [weak self] _ in
-                guard let self = self else { return }
-                self.textSubject.send(self.textView.text)
-                self.dismiss(animated: true)
-            })
-            .store(in: &subscriptions)
+            case let .done(text):
+                self?.textSubject.send(text)
+                self?.dismiss(animated: true)
+            }
+        }
+        .store(in: &subscriptions)
     }
 }
