24065e53312f042b44add5a7ec494e04e454f462
Michael Henry Pantaleon <me@iamkel.net>
2022-10-31 05:07:24 +1100

    update

--- Sources/XConfigs/Protocols/KeyValueProvider.swift
+++ Sources/XConfigs/Protocols/KeyValueProvider.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-// sourcery: AutoMockable
 public protocol KeyValueProvider {
     func get<Value: RawStringValueRepresentable>(for key: String) -> Value?
 }
--- Sources/XConfigs/Protocols/KeyValueStore.swift
+++ Sources/XConfigs/Protocols/KeyValueStore.swift
@@ -1,6 +1,5 @@
 import Foundation
 
-// sourcery: AutoMockable
 public protocol KeyValueStore {
     func get<Value: RawStringValueRepresentable>(for key: String) -> Value?
     func set<Value: RawStringValueRepresentable>(value: Value, for key: String)
