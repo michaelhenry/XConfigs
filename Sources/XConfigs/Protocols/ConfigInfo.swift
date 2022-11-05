63bb8e8e4c7cf13db596a324999d875585b7128f
Michael Henry <me@iamkel.net>
2022-11-05 19:23:16 +1100

    Update ConfigInfo.swift

--- Sources/XConfigs/Protocols/ConfigInfo.swift
+++ Sources/XConfigs/Protocols/ConfigInfo.swift
@@ -3,7 +3,6 @@ import Foundation
 public protocol ConfigInfo {
     var configKey: String { get }
     var configValue: RawStringValueRepresentable { get }
-    var configDefaultValue: RawStringValueRepresentable { get }
 
     var group: XConfigGroup { get }
 }
