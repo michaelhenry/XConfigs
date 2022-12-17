f7ff5c4cf7e1e7b3a821a8f507f5c55a1d02d09b
Michael Henry Pantaleon <me@iamkel.net>
2022-12-18 04:50:56 +1100

    section header

--- Sources/XConfigs/DataSources/AnyDiffableDataSource.swift
+++ Sources/XConfigs/DataSources/AnyDiffableDataSource.swift
@@ -2,16 +2,29 @@ import DiffableDataSources
 import Foundation
 import UIKit
 
+@available(iOS 13.0, *)
+private class TableViewDataSource<Section: Hashable, Item: Hashable>: UITableViewDiffableDataSource<Section, Item> {
+    override func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "\(snapshot().sectionIdentifiers[section])"
+    }
+}
+
+private class LegacyTableViewDataSource<Section: Hashable, Item: Hashable>: TableViewDiffableDataSource<Section, Item> {
+    override func tableView(_: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "\(snapshot().sectionIdentifiers[section])"
+    }
+}
+
 struct AnyDiffableDataSource<Section: Hashable, Item: Hashable> {
     private let datasource: Any?
 
     init(tableView: UITableView, cellProvider: @escaping (UITableView, IndexPath, Item) -> UITableViewCell?) {
         if #available(iOS 13.0, *) {
-            let ds = UITableViewDiffableDataSource<Section, Item>(tableView: tableView, cellProvider: cellProvider)
+            let ds = TableViewDataSource<Section, Item>(tableView: tableView, cellProvider: cellProvider)
             ds.defaultRowAnimation = .fade
             datasource = ds
         } else {
-            let ds = TableViewDiffableDataSource<Section, Item>(tableView: tableView, cellProvider: cellProvider)
+            let ds = LegacyTableViewDataSource<Section, Item>(tableView: tableView, cellProvider: cellProvider)
             ds.defaultRowAnimation = .fade
             datasource = ds
         }
@@ -19,20 +32,20 @@ struct AnyDiffableDataSource<Section: Hashable, Item: Hashable> {
 
     func applyAnySnapshot(_ snapshot: Any, animatingDifferences: Bool = true, completion: (() -> Void)? = nil) {
         if #available(iOS 13.0, *) {
-            guard let datasource = datasource as? UITableViewDiffableDataSource<Section, Item>, let snapshot = snapshot as? NSDiffableDataSourceSnapshot<Section, Item> else { return }
+            guard let datasource = datasource as? TableViewDataSource<Section, Item>, let snapshot = snapshot as? NSDiffableDataSourceSnapshot<Section, Item> else { return }
             datasource.apply(snapshot, animatingDifferences: animatingDifferences, completion: completion)
         } else {
-            guard let datasource = datasource as? TableViewDiffableDataSource<Section, Item>, let snapshot = snapshot as? DiffableDataSourceSnapshot<Section, Item> else { return }
+            guard let datasource = datasource as? LegacyTableViewDataSource<Section, Item>, let snapshot = snapshot as? DiffableDataSourceSnapshot<Section, Item> else { return }
             datasource.apply(snapshot, animatingDifferences: animatingDifferences, completion: completion)
         }
     }
 
     func itemIdentifier(for indexPath: IndexPath) -> Item? {
         if #available(iOS 13.0, *) {
-            guard let datasource = datasource as? UITableViewDiffableDataSource<Section, Item> else { return nil }
+            guard let datasource = datasource as? TableViewDataSource<Section, Item> else { return nil }
             return datasource.itemIdentifier(for: indexPath)
         } else {
-            guard let datasource = datasource as? TableViewDiffableDataSource<Section, Item> else { return nil }
+            guard let datasource = datasource as? LegacyTableViewDataSource<Section, Item> else { return nil }
             return datasource.itemIdentifier(for: indexPath)
         }
     }
