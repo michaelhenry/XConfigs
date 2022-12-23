6f44c49589565a2c266b880141ab93d7367790ec
Michael Henry Pantaleon <me@iamkel.net>
2022-12-23 23:46:13 +1100

    update to rxtests

--- Demo/DemoTests/XConfigsTests.swift
+++ Demo/DemoTests/XConfigsTests.swift
@@ -290,125 +290,133 @@ final class XConfigsTests: XCTestCase {
         let viewModel = XConfigsViewModel()
         defaultConfigUseCase.isOverriden = true
 
-        let reloadPublisher = PublishSubject<Void>()
-        let updateValuePublisher = PublishSubject<KeyValue>()
-        let resetPublisher = PublishSubject<Void>()
-        let output = viewModel.transform(input: .init(
-            reloadPublisher: reloadPublisher,
-            updateValuePublisher: updateValuePublisher,
-            overrideConfigPublisher: .empty(),
-            resetPublisher: resetPublisher
-        ))
+        let title = scheduler.createObserver(String.self)
+        let sectionItemsModels = scheduler.createObserver([SecItemsModel].self)
 
-        var sectionItemsModels = [[SectionItemsModel<ViewModel.Section, ViewModel.Item>]]()
-        output.sectionItemsModels.drive(onNext: { secItem in
-            sectionItemsModels.append(secItem)
-        })
-        .disposed(by: disposeBag)
+        output.title.drive(title).disposed(by: disposeBag)
+        output.sectionItemsModels.drive(sectionItemsModels).disposed(by: disposeBag)
 
         XCTAssertEqual(MockConfigs.shared.maxRetry, 10)
         XCTAssertEqual(MockConfigs.shared.maxRate, 1.0)
 
-        reloadPublisher.onNext(())
-        updateValuePublisher.onNext(.init(key: "maxRetry", value: "20"))
-        updateValuePublisher.onNext(.init(key: "maxRate", value: "0.99"))
-        resetPublisher.onNext(())
+        // MARK: INPUTS
 
-        XCTAssertEqual(sectionItemsModels[0], [
-            .init(section: .main, items: [
-                .overrideConfig(true),
-                .action("Reset"),
+        scheduler
+            .createColdObservable([
+                .next(0, ()),
+            ]).bind(to: reloadPublisher)
+            .disposed(by: disposeBag)
+
+        scheduler
+            .createColdObservable([
+                .next(1, .init(key: "maxRetry", value: "20")),
+                .next(2, .init(key: "maxRate", value: "0.99")),
+            ]).bind(to: updateValuePublisher)
+            .disposed(by: disposeBag)
+
+        scheduler
+            .createColdObservable([
+                .next(3, ()),
+            ]).bind(to: resetPublisher)
+            .disposed(by: disposeBag)
+
+        scheduler.start()
+
+        // MARK: - OUTPUTS
+
+        XCTAssertEqual(sectionItemsModels.events, [
+            .next(0, [
+                .init(section: .main, items: [
+                    .overrideConfig(true),
+                    .action("Reset"),
+                ]),
+                .init(section: .group(""), items: [
+                    .toggle(.init(key: "isOnboardingEnabled", value: true)),
+                    .textInput(.init(key: "apiURL", value: "https://prod.google.com")),
+                    .textInput(.init(key: "apiVersion", value: "v1.2.3")),
+                    .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
+                    .textInput(.init(key: "maxRetry", value: "10")),
+                    .textInput(.init(key: "threshold", value: "1")),
+                    .textInput(.init(key: "rate", value: "2.5")),
+                ]),
+                .init(section: .group("Feature 1"), items: [
+                    .textInput(.init(key: "maxScore", value: "100")),
+                    .textInput(.init(key: "maxRate", value: "1.0")),
+                ]),
+                .init(section: .group("Feature 2"), items: [
+                    .textInput(.init(key: "height", value: "44.0")),
+                    .textInput(.init(key: "width", value: "320.0")),
+                ]),
             ]),
-            .init(section: .group(""), items: [
-                .toggle(.init(key: "isOnboardingEnabled", value: true)),
-                .textInput(.init(key: "apiURL", value: "https://prod.google.com")),
-                .textInput(.init(key: "apiVersion", value: "v1.2.3")),
-                .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
-                .textInput(.init(key: "maxRetry", value: "10")),
-                .textInput(.init(key: "threshold", value: "1")),
-                .textInput(.init(key: "rate", value: "2.5")),
+            .next(1, [
+                .init(section: .main, items: [
+                    .overrideConfig(true),
+                    .action("Reset"),
+                ]),
+                .init(section: .group(""), items: [
+                    .toggle(.init(key: "isOnboardingEnabled", value: true)),
+                    .textInput(.init(key: "apiURL", value: "https://prod.google.com")),
+                    .textInput(.init(key: "apiVersion", value: "v1.2.3")),
+                    .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
+                    .textInput(.init(key: "maxRetry", value: "20")),
+                    .textInput(.init(key: "threshold", value: "1")),
+                    .textInput(.init(key: "rate", value: "2.5")),
+                ]),
+                .init(section: .group("Feature 1"), items: [
+                    .textInput(.init(key: "maxScore", value: "100")),
+                    .textInput(.init(key: "maxRate", value: "1.0")),
+                ]),
+                .init(section: .group("Feature 2"), items: [
+                    .textInput(.init(key: "height", value: "44.0")),
+                    .textInput(.init(key: "width", value: "320.0")),
+                ]),
             ]),
-            .init(section: .group("Feature 1"), items: [
-                .textInput(.init(key: "maxScore", value: "100")),
-                .textInput(.init(key: "maxRate", value: "1.0")),
+            .next(2, [
+                .init(section: .main, items: [
+                    .overrideConfig(true),
+                    .action("Reset"),
+                ]),
+                .init(section: .group(""), items: [
+                    .toggle(.init(key: "isOnboardingEnabled", value: true)),
+                    .textInput(.init(key: "apiURL", value: "https://prod.google.com")),
+                    .textInput(.init(key: "apiVersion", value: "v1.2.3")),
+                    .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
+                    .textInput(.init(key: "maxRetry", value: "20")),
+                    .textInput(.init(key: "threshold", value: "1")),
+                    .textInput(.init(key: "rate", value: "2.5")),
+                ]),
+                .init(section: .group("Feature 1"), items: [
+                    .textInput(.init(key: "maxScore", value: "100")),
+                    .textInput(.init(key: "maxRate", value: "0.99")),
+                ]),
+                .init(section: .group("Feature 2"), items: [
+                    .textInput(.init(key: "height", value: "44.0")),
+                    .textInput(.init(key: "width", value: "320.0")),
+                ]),
             ]),
-            .init(section: .group("Feature 2"), items: [
-                .textInput(.init(key: "height", value: "44.0")),
-                .textInput(.init(key: "width", value: "320.0")),
+            .next(3, [
+                .init(section: .main, items: [
+                    .overrideConfig(true),
+                    .action("Reset"),
+                ]),
+                .init(section: .group(""), items: [
+                    .toggle(.init(key: "isOnboardingEnabled", value: true)), // uses remote
+                    .textInput(.init(key: "apiURL", value: "https://prod.google.com")), // uses remote
+                    .textInput(.init(key: "apiVersion", value: "v1.2.3")),
+                    .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
+                    .textInput(.init(key: "maxRetry", value: "10")),
+                    .textInput(.init(key: "threshold", value: "1")),
+                    .textInput(.init(key: "rate", value: "2.5")),
+                ]),
+                .init(section: .group("Feature 1"), items: [
+                    .textInput(.init(key: "maxScore", value: "100")),
+                    .textInput(.init(key: "maxRate", value: "1.0")),
+                ]),
+                .init(section: .group("Feature 2"), items: [
+                    .textInput(.init(key: "height", value: "44.0")),
+                    .textInput(.init(key: "width", value: "320.0")),
+                ]),
             ]),
         ])
-
-        XCTAssertEqual(sectionItemsModels[1], [
-            .init(section: .main, items: [
-                .overrideConfig(true),
-                .action("Reset"),
-            ]),
-            .init(section: .group(""), items: [
-                .toggle(.init(key: "isOnboardingEnabled", value: true)),
-                .textInput(.init(key: "apiURL", value: "https://prod.google.com")),
-                .textInput(.init(key: "apiVersion", value: "v1.2.3")),
-                .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
-                .textInput(.init(key: "maxRetry", value: "20")),
-                .textInput(.init(key: "threshold", value: "1")),
-                .textInput(.init(key: "rate", value: "2.5")),
-            ]),
-            .init(section: .group("Feature 1"), items: [
-                .textInput(.init(key: "maxScore", value: "100")),
-                .textInput(.init(key: "maxRate", value: "1.0")),
-            ]),
-            .init(section: .group("Feature 2"), items: [
-                .textInput(.init(key: "height", value: "44.0")),
-                .textInput(.init(key: "width", value: "320.0")),
-            ]),
-        ])
-
-        XCTAssertEqual(sectionItemsModels[2], [
-            .init(section: .main, items: [
-                .overrideConfig(true),
-                .action("Reset"),
-            ]),
-            .init(section: .group(""), items: [
-                .toggle(.init(key: "isOnboardingEnabled", value: true)),
-                .textInput(.init(key: "apiURL", value: "https://prod.google.com")),
-                .textInput(.init(key: "apiVersion", value: "v1.2.3")),
-                .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
-                .textInput(.init(key: "maxRetry", value: "20")),
-                .textInput(.init(key: "threshold", value: "1")),
-                .textInput(.init(key: "rate", value: "2.5")),
-            ]),
-            .init(section: .group("Feature 1"), items: [
-                .textInput(.init(key: "maxScore", value: "100")),
-                .textInput(.init(key: "maxRate", value: "0.99")),
-            ]),
-            .init(section: .group("Feature 2"), items: [
-                .textInput(.init(key: "height", value: "44.0")),
-                .textInput(.init(key: "width", value: "320.0")),
-            ]),
-        ])
-
-        XCTAssertEqual(sectionItemsModels[3], [
-            .init(section: .main, items: [
-                .overrideConfig(true),
-                .action("Reset"),
-            ]),
-            .init(section: .group(""), items: [
-                .toggle(.init(key: "isOnboardingEnabled", value: true)), // uses remote
-                .textInput(.init(key: "apiURL", value: "https://prod.google.com")), // uses remote
-                .textInput(.init(key: "apiVersion", value: "v1.2.3")),
-                .optionSelection(.init(key: "region", value: "north", choices: regionChoices)),
-                .textInput(.init(key: "maxRetry", value: "10")),
-                .textInput(.init(key: "threshold", value: "1")),
-                .textInput(.init(key: "rate", value: "2.5")),
-            ]),
-            .init(section: .group("Feature 1"), items: [
-                .textInput(.init(key: "maxScore", value: "100")),
-                .textInput(.init(key: "maxRate", value: "1.0")),
-            ]),
-            .init(section: .group("Feature 2"), items: [
-                .textInput(.init(key: "height", value: "44.0")),
-                .textInput(.init(key: "width", value: "320.0")),
-            ]),
-        ])
-        XCTAssertEqual(sectionItemsModels.count, 4)
     }
 }
