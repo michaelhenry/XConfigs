bd93a76b79beb22b756466a396181fbb8726f4e4
KEL <me@iamkel.net>
2023-06-05 20:28:26 +1000

    Delete setup-swiftformat.sh

--- setup-swiftformat.sh
+++
@@ -1,5 +0,0 @@
-brew install swiftformat && \
-npm install --global git-format-staged && \
-echo '#!/bin/bash\ngit-format-staged --formatter "swiftformat stdin --stdinpath \x27{}\x27" "*.swift"' > '.git/hooks/pre-commit' && \
-chmod +x .git/hooks/pre-commit && \
-git config --global core.hooksPath '~/.githooks' 
